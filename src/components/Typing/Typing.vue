a5eda219ad3769efaa6c217d79b03cda11356b1d
kingkazuma7 <your.nasionasio7@gmail.com>
2025-02-13 19:15:17 +0900

    // input入力と配列のn番目の値が一致したら次に切り替え

--- src/components/Typing/Typing.vue
+++ src/components/Typing/Typing.vue
@@ -4,9 +4,9 @@
     <button v-if="startFlg != true" @click="gameStart()">スタート</button>
     <!-- trueのときのみ問題分を表示 -->
     <div v-if="startFlg">
-      <div class="question">apple</div>
+      <div class="question">{{ current_question }}</div>
       <div class="clear">Clear!</div>
-      <input type="text" class="">
+      <input v-model="typeBox" type="text" class="">
       <div class="gauge"></div>
       <div>1/5</div>
     </div>
@@ -14,8 +14,31 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const startFlg = ref(false);
+import { onMounted, ref, watch } from 'vue'
+const startFlg = ref(false); // 問題開始ステータス
+const current_question = ref(''); // 現在のタイプ文字
+const typeBox = ref('');
+
+const questions = [
+  'apple',
+  'banana',
+  'orange',
+  'grape',
+  'cherry',
+  'pear',
+  'pineapple'
+];
+
+onMounted(() => {
+  current_question.value = questions[2]
+})
+
+watch(typeBox, (newValue) => {
+  if (newValue === current_question.value) {
+    alert("一致した!");
+  }
+});
+
 
 const gameStart = () => {
   startFlg.value = true;
