941815b41ccf04ef34b27af3089e6f49bc3ca963
kingkazuma7 <your.nasionasio7@gmail.com>
2025-02-13 19:02:11 +0900

    startクリックで問題分表示 - startFlgで操作

--- src/components/Typing/Typing.vue
+++ src/components/Typing/Typing.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="typing-game">
     <h2>タイピングゲーム</h2>
+    <button v-if="startFlg != true" @click="gameStart()">スタート</button>
+    <!-- trueのときのみ問題分を表示 -->
+    <div v-if="startFlg">
+      <div class="question">apple</div>
+      <div class="clear">Clear!</div>
+      <input type="text" class="">
+      <div class="gauge"></div>
+      <div>1/5</div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+const startFlg = ref(false);
+
+const gameStart = () => {
+  startFlg.value = true;
+  console.log(startFlg.value);
+}
+
 </script>
 
 <style scoped>
-</style>e
+</style>
